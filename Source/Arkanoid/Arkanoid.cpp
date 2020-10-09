59d6281b964e63bea7dc79bbf756bfdd355f145d
Steven <stevenzhaooooo@gmail.com>
2020-10-09 23:10:51 +1300

    Fixing Project

--- Source/Arkanoid/Arkanoid.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Arkanoid.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arkanoid, "Arkanoid" );
